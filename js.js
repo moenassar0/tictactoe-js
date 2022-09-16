216c70eb39008c76d900acf01fea5ccaee59141d
Mohamad Nassar <mnassar57@gmail.com>
2022-09-16 21:13:21 +0300

    logic technically implemented

--- js.js
+++ js.js
@@ -1,13 +1,24 @@
 const grid_container = document.getElementById('grid-container');
 const rects = grid_container.children;
+const turnHeader = document.getElementById("turn");
+
 
 let x_turn = true;
-console.log(rects);
+
+let xpos = [];
+let opos = [];
 
 for(let i = 0; i < rects.length; i++){
     rects[i].setAttribute("id", i);
     rects[i].addEventListener("click", clicked)
 }
+
+const winningPositions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8]
+]
+
 
 let positionsOnBoard = [
     [0, null],
@@ -22,20 +33,38 @@ let positionsOnBoard = [
 ];
 
 function clicked(){
-    let id = this.getAttribute("id");
-    console.log(id);
-    if(x_turn == true){
+    turnHeader.innerHTML = "Turn: " + x_turn;
+    let id = parseInt(this.getAttribute("id"));
+    //console.log(id);
+    if(x_turn == true && positionsOnBoard[id][1] == null){
         positionsOnBoard[id][1] = 'x';
+        xpos.push(id);
         x_turn = false;
     }
-    else{
+    if(x_turn == false && positionsOnBoard[id][1] == null){
         positionsOnBoard[id][1] = 'o';
+        opos.push(id);
         x_turn = true;
     }
-    console.log(positionsOnBoard);
-    checkForWinning();
+    //console.log(positionsOnBoard);
+    //console.log(winningPositions[0]);
+    console.log(xpos, opos);
+    if(checkForWinning3(opos)){
+        
+    }
+
 }
 
-function checkForWinning(){
-
+function checkForWinning3(pos){
+    for(let y = 0; y < 3; y++){
+        let ar1 = winningPositions[y];
+        let ar2 = pos;
+        if(ar1.every(r => ar2.includes(r))){
+            console.log('Found all of', ar1, 'in', ar2);
+          }else{
+            console.log('Did not find all of', ar1, 'in', ar2);
+          }
+    }
 }
+
+
